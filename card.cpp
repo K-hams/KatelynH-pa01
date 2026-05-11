9a80a43fa823c3b5c952b78e59db00c6368c3775
Katelyn Hamel <Katelynhamel@cs.ucsb.edu>
2026-05-11 16:54:28 -0700

    message

--- card.cpp
+++ card.cpp
@@ -29,7 +29,7 @@ bool card::operator==(const card& other) const {
     
 }
 std::ostream& operator<<(std::ostream& os, const card& c) {
-    os << c.getValue() << " " << c.getName();
+    os << c.getName() << " " << c.getValue();
     return os;
 }
 
